7caf7dc7987b6d64bddf213c8bdccfd089916a2d
Noureddine Lardjani <noureddine.lardjani@gmail.com>
2022-01-26 15:08:45 +0100

    Initial commit

---
+++ src/main/java/io/javadev/springjavaproducer/SpringJavaProducerApplication.java
@@ -0,0 +1,13 @@
+package io.javadev.springjavaproducer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJavaProducerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJavaProducerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/javadev/springjavaproducer/SpringJavaProducerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javadev.springjavaproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJavaProducerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
